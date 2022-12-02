e314f4aefcdece26e41cff984d99d158501b8a53
thejas <thejaskala308@gmail.com>
2022-12-02 19:06:04 +0530

    make it better

--- components/ChannelCard.js
+++ components/ChannelCard.js
@@ -57,7 +57,7 @@ export default function ChannelCard({channel}) {
 
 	return(
 		<div 
-		onClick={joinChannel}
+		onClick={()=>{if(currentUser){joinChannel()}}}
 		className="flex w-full cursor-pointer ease-out transition duration-300
 		hover:bg-gray-700 p-2 py-4 rounded-xl justify-between items-center" >	
 			<div className="flex gap-2 items-center " >
